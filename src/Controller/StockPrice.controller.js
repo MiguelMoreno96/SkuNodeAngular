94c6da1d67b78bb71bff4ebb43f673148e7f3290
Alejandro <alexsotog82@gmail.com>
2021-04-22 11:43:08 -0500

    feat(alex): add new folder for Stock and price controller and model

---
+++ src/Controller/StockPrice.controller.js
@@ -0,0 +1,49 @@
+const StockPrice = require('../models/StockPrecie');
+
+
+
+
+exports.getAllStockPrice = async (req,res) => {
+   try {
+       const stockPrice = await StockPrice.find();
+
+       if(!stockPrice) return res.status(400).json({
+           success: false,
+           message: 'No hay datos de stock y precio'
+       })
+
+       return res.status(200).json({
+           success: true,
+           count: stockPrice.length,
+           data: stockPrice
+       })
+   } catch (error) {
+       return res.status(500).json({
+           success: false,
+           message: 'Erro en la peticion, Intentalo mas tarde'
+       })
+   }
+}
+
+exports.createStockPrice = async (req,res) => {
+    try{
+
+        const stockPrice = await StockPrice.create(req.body);
+
+        if(!StockPrice) return res.status(400).json({
+            success: false,
+            message: 'Error al Subir stock y precio'
+        })
+
+        return res.status(201).json({
+            success: true,
+            data: stockPrice
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Erro en la peticion, Intentalo mas tarde'
+        })
+    }
+}
---
+++ src/models/StockPrecie.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const StockPrecieSchema = new mongoose.Schema({
+    stock: {
+        type: Number,
+        required: [true, 'el stock es requerido para guardar'],
+        trim: true
+    },
+
+    price: {
+        type: Number,
+        required: [true, 'el precio es requerido para guardar'],
+        trim: true
+    },
+
+    pricePromotion: {
+        type: Number,
+        required: [true, 'el precio promoción es requerido para guardar'],
+        trim: true
+    },
+
+    sku: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SkuModel',
+        required: true
+    }
+});
+
+module.exports= mongoose.model("StockPrecie", StockPrecieSchema);
